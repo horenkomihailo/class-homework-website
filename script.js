420cc721b1a8ca556045a56eebe0ed7bf395d96b
horenkomihailo <122228656+horenkomihailo@users.noreply.github.com>
2024-09-16 20:36:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ document.getElementById('loginBtn').addEventListener('click', () => {
     }
 });
 
-// Здесь можно добавить логику для переключения недель и отображения домашних заданий
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDwS8O0Y4u6Qr1E21h7O9ny4ionOWirIN4",
+  authDomain: "class-homework-website.firebaseapp.com",
+  projectId: "class-homework-website",
+  storageBucket: "class-homework-website.appspot.com",
+  messagingSenderId: "83260705506",
+  appId: "1:83260705506:web:9fee3788b19fbcaa866cbb",
+  measurementId: "G-3VGM5K3V20"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const database = firebase.database(app);
